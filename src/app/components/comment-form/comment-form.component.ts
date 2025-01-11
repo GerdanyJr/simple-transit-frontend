13b0cdb8809667e29ccd7ded1db46a8dc9dc7059
Gerdany Junior <Asa_lindo@hotmail.com>
2025-01-11 16:39:58 -0300

    feat: create comment form component

---
+++ src/app/components/comment-form/comment-form.component.ts
@@ -0,0 +1,31 @@
+import { Component, input, output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-comment-form',
+  imports: [
+    MatInputModule,
+    MatButtonModule,
+    FormsModule
+  ],
+  templateUrl: './comment-form.component.html',
+  styleUrl: './comment-form.component.scss'
+})
+export class CommentFormComponent {
+  comment = signal<string>("");
+  isUserLoggedIn = input.required<boolean>();
+  onCommentSubmit = output<string>();
+
+  constructor(private router: Router) { }
+
+  onSubmit() {
+    if (this.isUserLoggedIn()) {
+      this.onCommentSubmit.emit(this.comment());
+    } else {
+      this.router.navigate(["auth/login"]);
+    }
+  }
+}
