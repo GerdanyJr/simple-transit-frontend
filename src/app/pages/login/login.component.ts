b07de6a21f514e47f8c97f11daa1ea8fceb43194
Gerdany Junior <Asa_lindo@hotmail.com>
2025-01-12 17:41:19 -0300

    feat: improve error handling in login component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -7,6 +7,8 @@ import { MatInputModule } from '@angular/material/input';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { ToastrService } from 'ngx-toastr';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ValidationErrorResponse } from '../../types/Error';
 
 @Component({
   selector: 'app-login',
@@ -44,7 +46,25 @@ export class LoginComponent {
       )
       .subscribe({
         next: () => this.router.navigate(["/"]),
-        error: () => this.toastr.error("Erro", "Ocorreu um erro inesperado"),
+        error: (err: HttpErrorResponse) => {
+          switch (err.status) {
+            case 403:
+              this.toastr.error("Credenciais inválidas!", "Erro");
+              break;
+
+            case 400:
+              Object
+                .entries(err.error.errors as ValidationErrorResponse)
+                .forEach(([field, message]) =>
+                  this.toastr.error(message, `Erro no campo: ${field}`)
+                );
+              break;
+
+            default:
+              this.toastr.error("Ocorreu um erro inesperado", "Erro");
+              break;
+          }
+        },
       });
   }
 }
