89ffc1efa484e3cd918c2c57bfddfdf4666eb91c
Gerdany Junior <Asa_lindo@hotmail.com>
2025-01-12 14:40:52 -0300

    feat: create auth-layout component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,60 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ReportsComponent } from './pages/reports/reports.component';
+import { ReportComponent } from './pages/report/report.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { AppLayoutComponent } from './layout/app-layout/app-layout.component';
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "auth",
+        component: AuthLayoutComponent,
+        children: [
+            {
+                path: "login",
+                title: "Entrar",
+                component: LoginComponent
+            },
+            {
+                path: "signup",
+                title: "Cadastre-se",
+                component: SignupComponent
+            },
+            {
+                path: "**",
+                redirectTo: "login"
+            }
+        ]
+    },
+    {
+        path: "",
+        component: AppLayoutComponent,
+        children: [
+            {
+                path: "dashboard",
+                title: "Dashboard",
+                component: DashboardComponent
+            },
+            {
+                path: "reports",
+                title: "Ocorrências",
+                children: [
+                    {
+                        path: "",
+                        component: ReportsComponent
+                    },
+                    {
+                        path: ":id",
+                        component: ReportComponent
+                    }
+                ]
+            },
+            {
+                path: "**",
+                redirectTo: "dashboard"
+            }
+        ]
+    },
+];
---
+++ src/app/layout/auth-layout/auth-layout.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-auth-layout',
+  imports: [
+    MatCardModule,
+    RouterOutlet,
+  ],
+  templateUrl: './auth-layout.component.html',
+  styleUrl: './auth-layout.component.scss'
+})
+export class AuthLayoutComponent {
+
+}
