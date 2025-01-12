266ad70a7129d35dcf5a02ad023d9dbc8aa8e60f
Gerdany Junior <Asa_lindo@hotmail.com>
2025-01-12 17:41:09 -0300

    feat: improve error handling in signup component

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -4,8 +4,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
@@ -23,7 +25,11 @@ import { AuthService } from '../../services/auth.service';
 export class SignupComponent {
   signupForm: FormGroup;
 
-  constructor(private authService: AuthService) {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {
     this.signupForm = new FormGroup({
       name: new FormControl("", [Validators.required]),
       login: new FormControl("", [Validators.required, Validators.minLength(5)]),
@@ -38,6 +44,18 @@ export class SignupComponent {
         this.signupForm.value.name,
         this.signupForm.value.login,
         this.signupForm.value.password
-      );
+      ).subscribe({
+        next: () => {
+          this.toastr.success("Usuário registrado com sucesso", "Sucesso")
+          this.router.navigate(["/auth/login"])
+        },
+        error: (err: HttpErrorResponse) => {
+          if (err.status == 409) {
+            this.toastr.error(err.error.message, "Erro")
+          } else {
+            this.toastr.error("Ocorreu um erro inesperado", "Erro")
+          }
+        },
+      });;
   }
 }
